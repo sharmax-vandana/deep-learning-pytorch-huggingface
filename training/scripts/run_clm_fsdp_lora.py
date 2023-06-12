b385522157382b00afa83c4520dbdd8e1c7fdc84
philschmid <schmidphilipp1995@gmail.com>
2023-06-12 11:48:32 -0400

    added script

--- training/scripts/run_clm_fsdp_lora.py
+++ training/scripts/run_clm_fsdp_lora.py
@@ -1,9 +1,10 @@
 # https://gist.github.com/younesbelkada/8bb36332cd2147c070b52ab25878c78f
 import os
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Optional, Tuple
 
 import torch
+import torch.distributed as dist
 from datasets import load_dataset
 from peft import LoraConfig
 from transformers import (
@@ -13,6 +14,8 @@ from transformers import (
     HfArgumentParser,
     AutoTokenizer,
     TrainingArguments,
+    PreTrainedModel,
+    PreTrainedTokenizer,
 )
 from transformers import TrainerCallback, TrainingArguments, TrainerState, TrainerControl
 from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
@@ -52,108 +55,16 @@ class PeftSavingCallback(TrainerCallback):
 
 
 ########################################################################
-# This is a fully working simple example to use trl's RewardTrainer.
 #
-# This example fine-tunes any causal language model (GPT-2, GPT-Neo, etc.)
-# by using the RewardTrainer from trl, we will leverage PEFT library to finetune
-# adapters on the model.
+# Utility methods for peft
 #
 ########################################################################
 
 
-# Define and parse arguments.
-@dataclass
-class ScriptArguments:
-    """
-    These arguments vary depending on how many GPUs you have, what their capacity and features are, and what size model you want to train.
-    """
-
-    local_rank: Optional[int] = field(default=-1, metadata={"help": "Used for multi-gpu"})
-
-    per_device_train_batch_size: Optional[int] = field(default=4)
-    per_device_eval_batch_size: Optional[int] = field(default=1)
-    gradient_accumulation_steps: Optional[int] = field(default=4)
-    learning_rate: Optional[float] = field(default=2e-4)
-    weight_decay: Optional[int] = field(default=0.001)
-    max_seq_length: Optional[int] = field(default=512)
-    model_name: Optional[str] = field(
-        default="tiiuae/falcon-7b",
-        metadata={
-            "help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."
-        },
-    )
-    dataset_name: Optional[str] = field(
-        default="timdettmers/openassistant-guanaco",
-        metadata={"help": "The preference dataset to use."},
-    )
-    use_4bit: Optional[bool] = field(
-        default=True,
-        metadata={"help": "Activate 4bit precision base model loading"},
-    )
-    use_nested_quant: Optional[bool] = field(
-        default=False,
-        metadata={"help": "Activate nested quantization for 4bit base models"},
-    )
-    bnb_4bit_compute_dtype: Optional[str] = field(
-        default="float16",
-        metadata={"help": "Compute dtype for 4bit base models"},
-    )
-    bnb_4bit_quant_type: Optional[str] = field(
-        default="nf4",
-        metadata={"help": "Quantization type fp4 or nf4"},
-    )
-    num_train_epochs: Optional[int] = field(
-        default=1,
-        metadata={"help": "The number of training epochs for the reward model."},
-    )
-    fp16: Optional[bool] = field(
-        default=True,
-        metadata={"help": "Enables fp16 training."},
-    )
-    bf16: Optional[bool] = field(
-        default=False,
-        metadata={"help": "Enables bf16 training."},
-    )
-    gradient_checkpointing: Optional[bool] = field(
-        default=True,
-        metadata={"help": "Enables gradient checkpointing."},
-    )
-    optim: Optional[str] = field(
-        default="paged_adamw_32bit",
-        metadata={"help": "The optimizer to use."},
-    )
-    lr_scheduler_type: Optional[str] = field(
-        default="linear",
-        metadata={"help": "The lr scheduler"},
-    )
-
-
-parser = HfArgumentParser(ScriptArguments)
-script_args = parser.parse_args_into_dataclasses()[0]
-
-
-def create_and_prepare_model(args):
-    compute_dtype = getattr(torch, args.bnb_4bit_compute_dtype)
-
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=args.use_4bit,
-        bnb_4bit_quant_type=args.bnb_4bit_quant_type,
-        bnb_4bit_compute_dtype=compute_dtype,
-        bnb_4bit_use_double_quant=args.use_nested_quant,
-    )
-
-    if compute_dtype == torch.float16 and args.use_4bit:
-        major, _ = torch.cuda.get_device_capability()
-        if major >= 8:
-            print("=" * 80)
-            print("Your GPU supports bfloat16, you can accelerate training with the argument --bf16")
-            print("=" * 80)
-
-    device_map = {"": 0}
-
-    model = AutoModelForCausalLM.from_pretrained(
-        args.model_name, quantization_config=bnb_config, device_map=device_map, trust_remote_code=True
-    )
+def create_and_prepare_model(
+    model_id, use_4bit=False, use_8bit=False
+) -> Tuple[PreTrainedModel, LoraConfig, PreTrainedTokenizer]:
+    model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", trust_remote_code=True)
 
     peft_config = LoraConfig(
         lora_alpha=8,
@@ -163,35 +74,101 @@ def create_and_prepare_model(args):
         target_modules=["query_key_value", "dense", "dense_h_to_4h", "dense_4h_to_h"],
     )
 
-    tokenizer = AutoTokenizer.from_pretrained(script_args.model_name, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
     tokenizer.pad_token = tokenizer.eos_token
 
     return model, peft_config, tokenizer
 
 
-training_arguments = TrainingArguments(
-    output_dir="./results",
-    per_device_train_batch_size=script_args.per_device_train_batch_size,
-    gradient_accumulation_steps=script_args.gradient_accumulation_steps,
-    optim=script_args.optim,
-    save_steps=1,
-    logging_steps=1,
-    learning_rate=script_args.learning_rate,
-    fp16=script_args.fp16,
-    bf16=script_args.bf16,
-)
+########################################################################
+# This is a fully working simple example to use trl's SFTTrainer.
+#
+# This example fine-tunes Falcon 40B on the Dolly dataset.
+#
+########################################################################
 
-model, peft_config, tokenizer = create_and_prepare_model(script_args)
-model.config.use_cache = False
-dataset = load_dataset(script_args.dataset_name, split="train")
 
-trainer = SFTTrainer(
-    model=model,
-    train_dataset=dataset,
-    peft_config=peft_config,
-    dataset_text_field="text",
-    max_seq_length=script_args.max_seq_length,
-    tokenizer=tokenizer,
-    args=training_arguments,
-)
-trainer.train()
+def format_dolly(sample):
+    instruction = f"### Instruction\n{sample['text']}"
+    context = f"### Context\n{sample['context']}" if len(sample["context"]) > 0 else None
+    response = f"### Answer\n{sample['answer']}"
+    # join all the parts together
+    prompt = "\n\n".join([i for i in [instruction, context, response] if i is not None])
+    return prompt
+
+
+# Define and parse arguments.
+@dataclass
+class ScriptArguments:
+    """
+    These arguments vary depending on how many GPUs you have, what their capacity and features are, and what size model you want to train.
+    """
+
+    model_id: Optional[str] = field(
+        metadata={
+            "help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."
+        },
+    )
+    dataset_id: Optional[str] = field(
+        metadata={
+            "help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."
+        },
+    )
+    max_seq_length: Optional[int] = field(
+        default=2048, metadata={"help": "The maximum sequence length for the SFTTainer to group together."}
+    )
+    local_rank: Optional[int] = field(default=-1, metadata={"help": "Used for multi-gpu"})
+
+
+def main():
+    parser = HfArgumentParser([ScriptArguments, TrainingArguments])
+    script_args, training_args = parser.parse_args_into_dataclasses()
+
+    # load model and tokenizer
+    model, peft_config, tokenizer = create_and_prepare_model(script_args)
+    # deactivate cache
+    model.config.use_cache = (
+        False if training_args.gradient_checkpointing else True,
+    )  # this is needed for gradient checkpointing
+    # load dataset
+    dataset = load_dataset(script_args.dataset_id, split="train")
+
+    # create trainer
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=dataset,
+        peft_config=peft_config,
+        packing=True,
+        max_seq_length=script_args.max_seq_length,
+        tokenizer=tokenizer,
+        formatting_func=format_dolly,  # formatting function
+        args=training_args,
+        callbacks=[PeftSavingCallback()],
+    )
+    # train
+    trainer.train()
+
+    # save model
+    dist.barrier()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# python run_clm_fsdp_lora.py \
+#  --model_id tiiuae/falcon-7b \
+#  --dataset_id "databricks/databricks-dolly-15k"
+#  --per_device_train_batch_size 1 \
+#  --epochs 1 \
+#  --optimizer adamw_apex_fused \
+#  --lr 2e-4 \
+#  --gradient_checkpointing True \
+#  --bf16 True \
+#  --tf32 True \
+#  --output_dir ./tmp \
+#  --logging_steps 10
+
+# \
+#  --fsdp "full_shard auto_wrap" \
+#  --fsdp_transformer_layer_cls_to_wrap "DecoderLayer"
