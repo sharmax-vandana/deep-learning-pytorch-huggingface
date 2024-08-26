f50ef7cc34570d80410ca0feb01e22d2692e5276
philschmid <schmidphilipp1995@gmail.com>
2024-08-26 13:01:04 +0000

    wip

--- inference/oai_load.js
+++ inference/oai_load.js
@@ -1,51 +1,131 @@
 import { check } from 'k6';
 import http from 'k6/http';
 import { scenario } from 'k6/execution';
-import { SharedArray } from 'k6/data';
+
+// curl -L -o samples.json https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
+// 1. run TGI then k6s
+// k6 run -e VU=1 -e DATA=samples.json oai_load.js
 
 // Define configurations
 const vu = __ENV.VU || 1;
 const data = __ENV.DATA || 'samples.json';
+const host = __ENV.HOST || '127.0.0.1:8080';
+const model_id = __ENV.MODEL_ID || 'tgi';
+const max_new_tokens = __ENV.MAX_NEW_TOKENS || 200;
 
-const samples = new SharedArray('ShareGPT samples', function () {
-  return JSON.parse(open(data));
-});
+const samples = JSON.parse(open(data))
 
-export const options = {
-  thresholds: {
-    http_req_failed: ['rate==0'],
-  },
-  scenarios: {
-    throughput: {
-      executor: 'shared-iterations',
-      vus: vu,
-      iterations: samples.length,
-      maxDuration: '90m',
-    },
-  },
-};
-
-export default function () {
-  // Load ShareGPT random example
-  const sample = samples[scenario.iterationInTest];
-  // Create Body 
-  const payload = {
-    inputs: sample.inputs,
-    parameters: {
-      max_new_tokens: 512,
-      details: true,
-      sample: true,
-      top_p: 0.9,
-      top_k: 50,
-      temperature: 0.2,
+export function get_options() {
+  return {
+    scenarios: {
+      load_test: {
+        executor: 'constant-arrival-rate',
+        duration: '30s',
+        preAllocatedVUs: vu,
+        rate: 1,
+        timeUnit: '1s',
+      },
     },
   };
-
-  const headers = { 'Content-Type': 'application/json' };
-  const res = http.post("http://localhost:8080/generate", JSON.stringify(payload), {
-    headers, timeout: '20m'
-  });
-  check(res, {
-    'Post status is 200': (r) => res.status === 200,
-  });
 }
+
+// export function handleSummary(data) {
+//   return {
+//     'summary.json': JSON.stringify(data),
+//   };
+// }
+
+
+function generate_payload(data, max_new_tokens) {
+  let input = data["conversations"][0]["value"];
+  if (input.length > 500) {
+    input = input.slice(0, 500)
+  }
+  return {
+    "messages": [{ "role": "user", "content": input }],
+    "temperature": 0,
+    "model": `${model_id}`,
+    "max_tokens": max_new_tokens,
+    "stream": true,
+  };
+}
+
+export const options = get_options();
+
+
+export default function run() {
+  const headers = { 'Content-Type': 'application/json' };
+  const query = samples[scenario.iterationInTest % samples.length];
+  const payload = JSON.stringify(generate_payload(query, max_new_tokens));
+
+  const url = `http://${host}/v1/chat/completions`;
+
+  const startTime = Date.now();
+  // let firstTokenTime = null;
+  // let lastTokenTime = null;
+  // let tokensCount = 0;
+  // let response = ""
+
+  // TODO: ADD SSE 
+  const res = http.post(url, payload, { headers: headers });
+
+  if (res.status >= 400 && res.status < 500) {
+    return;
+  }
+
+  check(res, {
+    'Post status is 200': (res) => res.status === 200,
+  });
+
+}
+
+
+// const res = sse.open(url, params, function (client) {
+//   client.on('event', function (event) {
+//     // console.log(event.data)
+//     if (parseInt(event.id) === 4) {
+//       client.close()
+//     }
+//     if (event.data.includes("[DONE]")) {
+//       return
+//     }
+//     try {
+//       const data = JSON.parse(event.data);
+//       const content = data['choices'][0]['delta']['content']
+//       if (content !== undefined) {
+//         response += data['choices'][0]['delta']['content']
+//       }
+//       tokensCount += 1;
+
+//       // Measure time to first token
+//       if (!firstTokenTime) {
+//         firstTokenTime = Date.now();
+//         timeToFirstToken.add(firstTokenTime - startTime);
+//       }
+
+//       // Measure inter-token latency
+//       const currentTime = Date.now();
+//       if (lastTokenTime) {
+//         interTokenLatency.add(currentTime - lastTokenTime);
+//       }
+//       lastTokenTime = currentTime;
+
+//       if ('finish_reason' in data['choices'][0]) {
+//         const endTime = Date.now();
+//         const deltaMs = endTime - startTime;
+//         endToEndLatency.add(deltaMs)
+//         requestThroughput.add(1);
+//         tokenThroughputPerSec.add(tokensCount / deltaMs * 1000);
+//         tokensReceived.add(tokensCount);
+//         // console.log(response)
+//       }
+//     } catch (e) {
+//       console.error('An unexpected error occurred: ', e)
+//       console.log(event)
+//     }
+//   })
+
+//   client.on('error', function (e) {
+//     console.log('An unexpected error occurred: ', e.error())
+//   })
+// })
--- inference/speculative.md
+++ inference/speculative.md
@@ -2,9 +2,12 @@
 
 ## MLP Speculator
 ```bash
-sudo docker run --gpus all -ti --shm-size 1g --ipc=host --rm -p 8080:80 \
   -e MODEL_ID=ibm-fms/llama3-8b-accelerator \
-  -e NUM_SHARD=1 \
+
+
+sudo docker run --gpus 2 -ti --shm-size 1g --ipc=host --rm -p 8080:80 \
+  -e MODEL_ID=meta-llama/Meta-Llama-3-8B-Instruct \
+  -e NUM_SHARD=2 \
   -e MAX_INPUT_TOKENS=1562 \
   -e MAX_TOTAL_TOKENS=2048 \
   -e HF_TOKEN=$(cat ~/.cache/huggingface/token) \
@@ -64,3 +67,23 @@ curl localhost:8080/v1/chat/completions \
 }' \
     -H 'Content-Type: application/json'
 ```
+
+# Benchmark
+
+1. Run TGI container with the following command:
+2. Run K6s benchmark with the following command:
+  
+  ```bash 
+  k6 run -e VU=10 -e DATA=samples.json oai_load.js
+  ```
+
+# Results
+
+`Acceleration = (Total Tokens (Count + Sum)) / (Number of Loops/Count)`
+
+| Model                               | vu  | Throughput (req/s) | Token Sum | Tokens Count | Total Tokens | Acceleration |
+| ----------------------------------- | --- | ------------------ | --------- | ------------ | ------------ | ------------ |
+| ibm-fms/llama3-8b-accelerator       | 1   | 0.250124/s         | 783       | 1067         | 1850         | 1.73         |
+| ibm-fms/llama3-8b-accelerator       | 10  | 0.934835/s         | 2953      | 3885         | 6838         | 1.76         |
+| meta-llama/Meta-Llama-3-8B-Instruct | 1   | 0.23309/s          | 0         | 1400         | 1400         | 1.00         |
+| meta-llama/Meta-Llama-3-8B-Instruct | 10  | 0.913191/s         | 0         | 6408         | 6408         | 1.00         |
